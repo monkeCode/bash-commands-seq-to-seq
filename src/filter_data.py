83d5cdc1feaf249883a7947d59b5d23e2c867b3d
Georgy <georgy.tertychniy@yandex.ru>
2025-09-23 10:44:10 +0300

    new data

--- src/filter_data.py
+++ src/filter_data.py
@@ -5,15 +5,7 @@ big_frame = pd.read_csv("data/generated_data.csv")
 big_frame = big_frame.dropna()
 big_frame = big_frame[big_frame.command.apply(lambda x:x[0]!="#")]
 
-with open("data/chatGPT_generated_data.json") as f:
-    d = f.read()
-    j = json.loads(d)
+internet_frame = pd.read_csv("data/inet_dataset.csv")
 
 
-gpt_frame = pd.DataFrame(j.values()).rename(columns={"invocation":"description", "cmd":"command"})
-
-
-nl2bash_frame = pd.read_csv("data/nl2bash.csv").rename(columns={"nl_command":"description", "bash_code":"command"})
-
-
-pd.concat([big_frame[["command", "description"]], gpt_frame[["command", "description"]], nl2bash_frame[["command", "description"]]]).to_csv("data/train.csv", index=None)
+pd.concat([big_frame[["command", "description"]], internet_frame[["command", "description"]]]).to_csv("data/train.csv", index=None)
